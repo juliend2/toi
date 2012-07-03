c5fb78befa6219360ec965edb6496345a82f2cef
Julien Desrosiers <juliend2@gmail.com>
2012-07-03 14:10:23 -0400

    we dont need to set remote_pwd if host is empty

--- lib/procs.tcl
+++ lib/procs.tcl
@@ -1,5 +1,7 @@
 proc set_remote_pwd {} {
   global host remote_pwd
-  set remote_pwd [exec ssh $host "'pwd'"]
+  if { $host != "" } {
+    set remote_pwd [exec ssh $host "'pwd'"]
+  }
   return $remote_pwd
 }
