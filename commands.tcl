2c13d52aa23ebcd33ea939e16f06cadd2515065e
Julien Desrosiers <juliend2@gmail.com>
2012-07-02 22:39:51 -0400

    start the in command

--- commands.tcl
+++ commands.tcl
@@ -1,4 +1,16 @@
 proc ssh {command} {
-  global host
-  exec ssh $host "$command"
+  global host remote_pwd
+  set retval [exec ssh $host "$command"]
+  puts $retval
+  return $retval
+}
+
+proc in {directory body} {
+  global remote_pwd
+  set prev_remote_pwd $directory
+  set remote_pwd $directory
+  # execute the inner block:
+  uplevel 1 $body
+  # return to previous directory
+  set remote_pwd $prev_remote_pwd
 }
